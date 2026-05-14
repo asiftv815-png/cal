098caa035fe132ddca9e47e9effbfde102b1ab1c
asiftv815-png <asiftv815@gmail.com>
2026-05-14 22:20:57 +0600

    Removed duplicate aman_56_calory_counter project

--- aman_56_calory_counter/calory_counter/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from calory_counter.models import *
-
-# Register your models here.
-admin.site.register([User, CaloryModel, BasicInfoModel])
-
--- aman_56_calory_counter/calory_counter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CaloryCounterConfig(AppConfig):
-    name = 'calory_counter'
